db32233caedab3cdcfd4acda33ebc4bf2205f244
Manav sambyal <manavsambyal2017@gmail.com>
2020-01-12 19:37:42 +0530

    DOne

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Searchbar from './Components/Searchbar'
+
+export default class App extends Component{
+
+    render(){
+        return(
+            <div className = "App">
+            <Searchbar />
+            </div>
+        )
+    }
+}
